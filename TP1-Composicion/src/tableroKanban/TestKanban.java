1d2376ad4c6ec6a8d3b096b0c805d7a44f4dc51e
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2021-10-22 11:14:11 -0300

    add TP1-Composicion add tableroKanban

---
+++ TP1-Composicion/src/tableroKanban/TestKanban.java
@@ -0,0 +1,14 @@
+package tableroKanban;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestKanban {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
